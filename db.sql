6d28caa883fb2b6ae8d5fd46e79ae8727f0fffc2
NguyenMTue <23115053122342@sv.ute.udn.vn>
2026-04-07 21:43:38 +0700

    done

---
+++ db.sql
@@ -0,0 +1,18 @@
+﻿CREATE DATABASE StudentDB;
+GO
+
+USE StudentDB;
+GO
+
+CREATE TABLE Students (
+    Id INT PRIMARY KEY IDENTITY(1,1),
+    FullName NVARCHAR(100) NOT NULL,
+    Age INT,
+    Email NVARCHAR(100)
+);
+
+-- Chèn dữ liệu mẫu
+INSERT INTO Students (FullName, Age, Email) VALUES 
+(N'Nguyen Van A', 20, 'a.nguyen@example.com'),
+(N'Tran Thi B', 22, 'b.tran@example.com');
+GO
