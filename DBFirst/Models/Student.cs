5e2b42ac655cdd273aad43395b59fba800e0e758
NguyenMTue <23115053122342@sv.ute.udn.vn>
2026-04-07 20:42:35 +0700

    B3: genera dbcontext and model

---
+++ DBFirst/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DBFirst.Models;
+
+public partial class Student
+{
+    public int Id { get; set; }
+
+    public string FullName { get; set; } = null!;
+
+    public int? Age { get; set; }
+
+    public string? Email { get; set; }
+}
